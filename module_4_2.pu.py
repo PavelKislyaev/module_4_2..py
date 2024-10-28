bf742e0461b6ea34745a3ca4a23567815dd1e3f6
PavelKislyaev <pavel.kislyayev@mail.ru>
2024-10-28 12:05:45 +0300

    Add files via upload

---
+++ module_4_2.pu.py
@@ -0,0 +1,15 @@
+def test_function():
+    def inner_function():
+        print("Я в области видимости функции test_function")
+
+    inner_function()
+
+
+test_function()
+
+
+def inner_function():
+    pass
+
+
+inner_function()
